9afcbf69182bbc64f1647e26e7511fde237a7cfc
Aaron Calkins <72696461+randomspareparts@users.noreply.github.com>
2026-02-20 12:37:59 -0500

    Update cache version from v6 to v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "rebuildt-scout-v6";
+const CACHE = "rebuildt-scout-v7";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
