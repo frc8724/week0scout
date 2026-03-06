372fcf73f91b1284f3f8f6b16961576e84a34792
Aaron Calkins <72696461+randomspareparts@users.noreply.github.com>
2026-03-06 12:15:23 -0500

    Update cache version from v11 to v13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "rebuildt-scout-v11";
+const CACHE = "rebuildt-scout-v13";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
